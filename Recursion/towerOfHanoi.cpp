732d8e0c81bd5727f4cd78dd4e3a248597212555
Moni Kaur <Kaurmoni0013@gmail.com>
2026-08-09 17:20:59 +0530

    Tower of hanoi

--- Recursion/towerOfHanoi.cpp
+++ Recursion/towerOfHanoi.cpp
@@ -1,17 +1,53 @@
+/*
+ * Problem: Tower of Hanoi
+ * Platform: GFG / Practice
+ * Topic: Recursion
+ * Difficulty: Easy
+ *
+ * Description:
+ * Move N disks from the source rod to the destination rod
+ * using a helper rod while following the rules of Tower of Hanoi.
+ *
+ * Rules:
+ * 1. Only one disk can be moved at a time.
+ * 2. Only the top disk can be moved.
+ * 3. A larger disk cannot be placed on a smaller disk.
+ *
+ * Number of moves required:
+ * 2^n - 1
+ */
+
 #include <iostream>
 using namespace std;
-void TOH(int n, int sor, int hel, int des)
+
+void TOH(int n, int source, int helper, int destination)
 {
+    // Base case
     if (n == 0)
         return;
-    TOH(n - 1, sor, des, hel);
-    cout << "move disk " << n << " from " << sor << " to " << des << endl;
-    TOH(n - 1, hel, sor, des);
+
+    // Move n-1 disks from source to helper
+    TOH(n - 1, source, destination, helper);
+
+    // Move the largest disk from source to destination
+    cout << "Move disk " << n
+         << " from " << source
+         << " to " << destination << endl;
+
+    // Move n-1 disks from helper to destination
+    TOH(n - 1, helper, source, destination);
 }
+
 int main()
 {
-    cout << "Enter no of disks :";
     int n;
+
+    cout << "Enter the number of disks: ";
     cin >> n;
+
+    cout << "\nSequence of moves:\n";
+
     TOH(n, 1, 2, 3);
+
+    return 0;
 }
